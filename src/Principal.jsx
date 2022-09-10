a2a2d03e54432f1ea7625c11d6672d3a8f21e8d4
Mateo Pineiro <mateopineiro2001@gmail.com>
2022-09-10 19:19:18 -0300

    fix to deploy

---
+++ src/Principal.jsx
@@ -0,0 +1,25 @@
+
+
+const Principal = () => {
+
+
+  return (
+    <div className="App">
+      <header>
+        <div>
+          <h1>Mateo Piñeiro</h1>
+        </div>
+        <img src="./src/assets/pic.png" alt="Dev's img"/>
+      </header>
+      <div class="proyects">
+          <h2>Some of my creations:</h2>
+          <a target="_blank" href="https://symphonious-pavlova-924168.netlify.app/"><img class="proyect" src="./src/assets/BusinessManager.png" alt="proyect"/></a>
+          <a target="_blank" href="https://meek-syrniki-76a53e.netlify.app/"><img class="proyect" src="./src/assets/pacientAdmin.png" alt=""/></a>
+          <a href=""></a>
+          <a href=""></a>
+      </div>
+    </div>
+  )
+}
+
+export default Principal
