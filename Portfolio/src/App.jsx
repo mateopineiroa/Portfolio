4ac66cafc06fc0562ea5a841d304bc376da89017
Mateo Pineiro <mateopineiro2001@gmail.com>
2022-09-10 18:38:02 -0300

    react and bg animated implemented

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import Principal from './Principal'
+import Background from './Background'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Background />
+      <Principal />
+    </>
+  )
+}
+
+export default App
---
+++ Portfolio/src/Background.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <div>
+      <div class="area" >
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div >
+    </div>
+  )
+}
+
+export default Background
