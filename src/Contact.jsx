6f2093482d07e1c772a747f97a467796092580b6
mateo <mateopineiro2001@gmail.com>
2022-09-15 11:16:31 -0300

    target a wsp web

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <div>
       
       <p><a href="mailto:john@example.com"><span class="material-symbols-outlined">mail</span>mateopineiro2001@gmail.com</a></p>
-      <p><a href="https://api.whatsapp.com/send?phone=59898557448&text=Estoy%20interesado/a%20en%20hablar">Link a wsp web</a></p>
+      <p><a target={"_blank"} href="https://api.whatsapp.com/send?phone=59898557448&text=Estoy%20interesado/a%20en%20hablar">Link a wsp web</a></p>
       <p><a target={"_blank"} href="https://github.com/mateopineiroa"><img src={gitHub} alt="github Img" /></a></p>
       {/* Form and handle form(?) */}
     </div>
