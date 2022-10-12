0dcb89a5b0bea158a98dfe773f3f0ee28ef05f14
mateo <mateopineiro2001@gmail.com>
2022-10-12 19:19:08 -0300

    contact cursor pinter

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -14,25 +14,25 @@ const Contact = () => {
     <div id="contact" className='flex flex-col w-screen h-screen justify-center'>
       <h3 className='self-center text-4xl'>Contact</h3>
       <div className="self-center grid grid-cols-2 md:grid-cols-4 w-1/3 mt-8 gap-9">
-        <div className="flex flex-col justify-center align-middle">
-          <Link href={"https://linkedin.com/in/mateopineiro"} target="_blank" rel="noreferrer">
+        <div className="flex flex-col justify-center align-middle cursor-pointer">
+          <a href={"https://linkedin.com/in/mateopineiro"} target="_blank" rel="noreferrer">
             <Image alt="" src={linkedin} objectFit="contain" width={300} height={300} />
-          </Link>
+          </a>
         </div>
-        <div className="flex flex-col justify-center align-middle">
-          <Link href={"https://github.com/mateopineiroa"} target="_blank" rel="noreferrer">
+        <div className="flex flex-col justify-center align-middle cursor-pointer">
+          <a href={"https://github.com/mateopineiroa"} target="_blank" rel="noreferrer">
             <Image alt="" src={github} objectFit="contain" width={300} height={300} />
-          </Link>
+          </a>
         </div>
-        <div className="flex flex-col justify-center align-middle">
-          <Link href={"https://api.whatsapp.com/send?phone=59898557448&text=Estoy%20interesado/a%20en%20hablar"} target="_blank" rel="noreferrer">
+        <div className="flex flex-col justify-center align-middle cursor-pointer">
+          <a href={"https://api.whatsapp.com/send?phone=59898557448&text=Estoy%20interesado/a%20en%20hablar"} target="_blank" rel="noreferrer">
             <Image alt="" src={wsp} objectFit="contain" width={300} height={300} />
-          </Link>
+          </a>
         </div>
-        <div className="flex flex-col justify-center align-middle">
-          <Link className="" href={"mailto:john@example.com"} rel="noreferrer">
+        <div className="flex flex-col justify-center align-middle cursor-pointer">
+          <a className="" href={"mailto:john@example.com"} rel="noreferrer">
             <Image alt="" src={mail} objectFit="contain" width={300} height={300} />
-          </Link>
+          </a>
           {/* <p className="w-full text-center">mateopineiro2001@gmail.com</p> */}
           <button className=" text-center w-full" onClick={copyToClipboard}>Copy to clipboard</button>
         </div>
