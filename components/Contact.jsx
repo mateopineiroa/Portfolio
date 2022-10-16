654ddd67c4427b3b62e66adf29cb100988a37155
Mateo Pineiro <mateopineiro2001@gmail.com>
2022-10-16 16:45:27 -0300

    portfolio img update

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
   return (
     <div id="contact" className='flex flex-col w-screen min-h-screen justify-center'>
       <h3 className='self-center text-4xl'>Contact</h3>
-      <div className="self-center grid grid-cols-2 md:grid-cols-4 w-full p-4 md:w-1/3 mt-8 gap-9">
+      <div className="self-center grid grid-cols-2 md:grid-cols-4 w-full p-4 lg:w-1/2 2xl:w-1/3 mt-8 gap-9">
         <div className="flex flex-col justify-center align-middle cursor-pointer">
           <a href={"https://linkedin.com/in/mateopineiro"} target="_blank" rel="noreferrer">
             <Image alt="" src={linkedin} objectFit="contain" width={300} height={300} />
@@ -23,13 +23,14 @@ const Contact = () => {
             <Image alt="" src={github} objectFit="contain" width={300} height={300} />
           </a>
         </div>
-        <div className="flex flex-col justify-center align-middle cursor-pointer">
+        <div className="flex flex-col justify-center align-middle">
           <a href={"https://api.whatsapp.com/send?phone=59898557448&text=Estoy%20interesado/a%20en%20hablar"} target="_blank" rel="noreferrer">
             <Image alt="" src={wsp} objectFit="contain" width={300} height={300} />
           </a>
+            <p className="text-center ">+598 98 557 448</p>
         </div>
-        <div className="flex flex-col justify-center align-middle cursor-pointer">
-          <a className="" href={"mailto:john@example.com"} rel="noreferrer">
+        <div className="flex flex-col justify-center align-middle ">
+          <a className="cursor-pointer" href={"mailto:mateopineiro2001@gmail.com"} target="_blank" rel="noreferrer">
             <Image alt="" src={mail} objectFit="contain" width={300} height={300} />
           </a>
           {/* <p className="w-full text-center">mateopineiro2001@gmail.com</p> */}
