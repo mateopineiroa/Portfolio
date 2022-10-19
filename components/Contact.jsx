18ad6d4c2aa4b44ba2c90d21d1cd722f7f40b991
mateo <mateopineiro2001@gmail.com>
2022-10-19 11:22:56 -0300

    fixed responsive iphone se nav && rm wsp

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = ({scrollIntro}) => {
   return (
     <div id="contact" className='flex flex-col w-screen min-h-screen justify-center'>
       <h3 className='self-center text-4xl mt-44'>Contact</h3>
-      <div className="self-center grid grid-cols-2 md:grid-cols-4 w-full p-4 lg:w-1/2  mt-8 mb-24 gap-9">
+      <div className="self-center grid grid-cols-2 md:grid-cols-3 w-full p-4 lg:w-1/2  mt-8 mb-24 gap-9">
         <div className="flex flex-col justify-center align-middle cursor-pointer">
           <a className="self-center" href={"https://linkedin.com/in/mateopineiro"} target="_blank" rel="noreferrer">
             <Image alt="" src={linkedin} objectFit="contain" width={300} height={300} />
@@ -24,14 +24,7 @@ const Contact = ({scrollIntro}) => {
             <Image alt="" src={github} objectFit="contain" width={300} height={300} />
           </a>
         </div>
-        <div className="flex flex-col justify-center align-middle">
-          <a className="self-center" href={"https://api.whatsapp.com/send?phone=59898557448&text=Estoy%20interesado/a%20en%20hablar"} target="_blank" rel="noreferrer">
-            <Image alt="" src={wsp} objectFit="contain" width={300} height={300} />
-          </a>
-            <p onClick={() => copyToClipboard("+598 98 557 448")} className="text-center self-center hover:cursor-pointer text-xs">+598 98 557 448<span className="material-symbols-outlined text-xs scale-75">
-            content_copy
-            </span></p>
-        </div>
+        
         <div className="flex flex-col justify-center align-middle">
           <a className="cursor-pointer self-center" href={"mailto:mateopineiro2001@gmail.com"} target="_blank" rel="noreferrer">
             <Image alt="" src={mail} objectFit="contain" width={300} height={300} />
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,14 +3,14 @@
 const Nav = ({ scrollAboutMe, scrollIntro, scrollSkills, scrollProjects, scrollContact }) => {
   
   return (
-    <div className='flex justify-end sticky z-50 top-0 bg-gray-100  pr-5'>
+    <div className='flex justify-end sticky z-50 top-0 bg-gray-100  pr-5 '>
 
       <div className="">
-        <button  className='hover:text-gray-500 transition-all ease my-3 p-3 font-bold text-gray-900' onClick={scrollIntro}>Intro</button>
-        <button  className='hover:text-gray-500 transition-all ease my-3 p-3 font-bold text-gray-900' onClick={scrollAboutMe}>About Me</button>
-        <button  className='hover:text-gray-500 transition-all ease my-3 p-3 font-bold text-gray-900' onClick={scrollSkills}>Skills</button>
-        <button  className='hover:text-gray-500 transition-all ease my-3 p-3 font-bold text-gray-900' onClick={scrollProjects}>Projects</button>
-        <button  className='hover:text-gray-500 transition-all ease my-3 p-3 font-bold text-gray-900' onClick={scrollContact}>Contact</button>
+        <button  className='hover:text-gray-500 transition-all ease my-3 p-3 text-xs sm:text-base font-bold text-gray-900' onClick={scrollIntro}>Intro</button>
+        <button  className='hover:text-gray-500 transition-all ease my-3 p-3 text-xs sm:text-base font-bold text-gray-900' onClick={scrollAboutMe}>About Me</button>
+        <button  className='hover:text-gray-500 transition-all ease my-3 p-3 text-xs sm:text-base font-bold text-gray-900' onClick={scrollSkills}>Skills</button>
+        <button  className='hover:text-gray-500 transition-all ease my-3 p-3 text-xs sm:text-base font-bold text-gray-900' onClick={scrollProjects}>Projects</button>
+        <button  className='hover:text-gray-500 transition-all ease my-3 p-3 text-xs sm:text-base font-bold text-gray-900' onClick={scrollContact}>Contact</button>
       </div>
     </div>
   )
